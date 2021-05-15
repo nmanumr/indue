9bb4d01692919b6a70097257ebab7f73dbd3d9b5
nmanumr <nmanumr@gmail.com>
2021-05-15 21:33:26 +0500

    adds props

--- components/monthSelector.tsx
+++ components/monthSelector.tsx
@@ -5,17 +5,25 @@ const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "
 
 interface State {
   years: number[],
-  selected: [number, number][],
+  selected: string[],
 }
 
-export default class MonthSelector extends Component<{}, State> {
+interface Props {
+  allowRange: boolean;
+  maxRangeLength: number;
+  min: string;
+  max: string;
+  onChange: (selection: string[]) => void;
+}
+
+export default class MonthSelector extends Component<Partial<Props>, State> {
   private yearElHeight: number = 0;
   private containerEl: RefObject<HTMLDivElement>;
   private preventScrollEvents = false;
   private sholdScrollToCurrentYear = false;
   private readonly YEARS_PAD = 5;
 
-  constructor(props: {}) {
+  constructor(props: Partial<Props>) {
     super(props);
 
     let currentYear = new Date().getFullYear();
@@ -23,7 +31,7 @@ export default class MonthSelector extends Component<{}, State> {
 
     this.state = {
       years: Array.from(Array(this.YEARS_PAD * 2 + 1).keys()).map(i => currentYear - this.YEARS_PAD + i),
-      selected: [[currentMonth, currentYear]],
+      selected: [`${currentYear}-${currentMonth}`],
     };
     this.containerEl = createRef();
   }
@@ -65,17 +73,18 @@ export default class MonthSelector extends Component<{}, State> {
     if (this.state.years[0] !== currentYear - this.YEARS_PAD) {
       this.setState({
         years: Array.from(Array(this.YEARS_PAD * 2 + 1).keys()).map(i => currentYear - this.YEARS_PAD + i),
-        selected: [[currentMonth, currentYear]]
+        selected: [`${currentYear}-${currentMonth}`]
       });
     } else {
-      // this.scrollToCurrentYear();
-      this.setState({selected: [[currentMonth, currentYear]]})
+      this.setState({selected: [`${currentYear}-${currentMonth}`]})
     }
     this.sholdScrollToCurrentYear = true;
   }
 
   handleMonthSelection([month, year]: [number, number]) {
-    this.setState({selected: [[month, year]]})
+    this.setState({selected: [`${year}-${month}`]});
+    if (this.props.onChange)
+      this.props.onChange([`${year}-${month}`]);
   }
 
   handleScroll(scrollTop: number) {
@@ -92,10 +101,31 @@ export default class MonthSelector extends Component<{}, State> {
     }
   }
 
+  handleNextMonth() {
+    let nextScrollHeight = (Math.floor((this.containerEl.current?.scrollTop ?? 0) / this.yearElHeight) + 1) * this.yearElHeight;
+    this.containerEl.current?.scrollTo(0, nextScrollHeight + 46);
+  }
+
+  handlePrevMonth() {
+    let nextScrollHeight = (Math.floor((this.containerEl.current?.scrollTop ?? 0) / this.yearElHeight) - 1) * this.yearElHeight;
+    this.containerEl.current?.scrollTo(0, nextScrollHeight + 46);
+  }
+
   render() {
     return (
       <div ref={this.containerEl} className="w-72 h-52 overflow-y-auto bg-white border border-gray-300 rounded-lg">
-        <div className="flex py-1.5 px-4 sticky top-0 z-10 bg-gray-50 border-b border-gray-200">
+        <div className="flex items-center py-1.5 px-1.5 sticky top-0 z-10 bg-gray-50 border-b border-gray-200">
+          <button onClick={this.handlePrevMonth.bind(this)} className="text-gray-600 pt-2 p-1.5 hover:bg-gray-100 rounded">
+            <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
+            </svg>
+          </button>
+          <div className="w-11"></div>
+          <button onClick={this.handleNextMonth.bind(this)} className="text-gray-600 ml-0.5 pt-2 p-1.5 hover:bg-gray-100 rounded">
+            <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+            </svg>
+          </button>
           <div className="flex-grow"></div>
           <button onClick={this.handleTodayBtn.bind(this)} className="text-gray-600 p-1.5 hover:bg-gray-100 rounded">
             <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -105,17 +135,17 @@ export default class MonthSelector extends Component<{}, State> {
         </div>
         {this.state.years.map((year) => {
           return <div className="relative" key={year} data-year={year}>
-            <span className="z-20 sticky top-2 px-6 py-1.5 text-sm font-medium text-gray-500">
+            <span className="z-20 sticky top-2 inline-block ml-11 pt-1.5 pb-1.5 text-sm font-medium text-gray-500">
               {year}
             </span>
-            <div className="px-6 grid grid-cols-4 gap-2 py-4 mt-4">
+            <div className="border-b border-gray-200 p-6 grid grid-cols-4 gap-2 mb-px">
               {months.map((month, index) => {
                 return <button
                   key={`${year}-${month}`}
                   onClick={() => this.handleMonthSelection([index, year])}
                   className={c(
                     "p-2 flex items-center justify-center rounded cursor-pointer uppercase text-sm",
-                    this.state.selected.find(([i, y]) => i === index && y === year)
+                    this.state.selected[0] == `${year}-${index}`
                       ? 'text-blue-600 bg-blue-100'
                       : 'text-gray-700 hover:bg-gray-100'
                   )}
